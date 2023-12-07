71046cabea2e0f7f18fc69f34ced5f7816b1924d
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-12-07 13:49:30 +0700

    feat: add cors

--- mood_based_recommendation/app/main.py
+++ mood_based_recommendation/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.mood import router as mood_router
 from app.routes.user import router as auth_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=[""],
+)
 
 @app.get("/")
 async def root():
